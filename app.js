f60fc9db89264f35b82b1de7420f12a1162f6010
LiveTheFuture <81014027+LiveTheFuture@users.noreply.github.com>
2021-08-21 17:46:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const exphbs = require ('express-handlebars');
 const nodemailer = require ('nodemailer');
 const { google } = require ('googleapis');
 const app = express();
@@ -78,6 +77,6 @@ async function sendMail() {
 })
 
 
-app.listen(5000, (req, res) => {
-    console.log(`Server is listening on port 5000`)
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
